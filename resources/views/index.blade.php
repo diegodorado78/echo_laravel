1cf08c599df13e1bfe752a29b3f2a15a75ef3bce
diego dorado <diegodorado78@gmail.com>
2021-02-04 20:22:14 -0500

    primer_setup diego

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.layout')
+
+@section('content')
+<div class="text-center" >
+  <h1 class="text-2xl text-gray-700 mb-2 uppercase">ECHO LEARNING</h1>
+  <h2 class="text-xl text-gray-600">Contacta los mejores profesionales en tecnología</h2>
+  <h3 class="text-lg text-gray-600">refuerza tus conocimientos y adquiere muchos mas</h3>
+</div>
+@endsection
