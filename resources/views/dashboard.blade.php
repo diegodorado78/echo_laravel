630925556dfff151d4f2a3675ea40f16ff442f05
diego dorado <diegodorado78@gmail.com>
2021-04-05 23:47:38 -0500

    funcional adminlte

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Bienvenido Diego ') }}
+        <h2 class="font-bold text-xl text-gray-800 leading-tight">
+            {{ __('Bienvenido a tu perfil') }}
         </h2>
     </x-slot>
 
