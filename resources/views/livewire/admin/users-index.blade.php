093c1b9a09ed5033cfb5567e7ac4fc22ac17c724
diego dorado <diegodorado78@gmail.com>
2021-06-16 18:15:51 -0500

    funcional 16-junio

--- resources/views/livewire/admin/users-index.blade.php
+++ resources/views/livewire/admin/users-index.blade.php
@@ -1,12 +1,12 @@
 <div>
     <div class="card">
         {{-- card header --}}
-<div class="card-header">
+<div class="card-header ">
     <input wire:model="search" type="text" class="form-control" placeholder="ingrese nombre o correo de usuario">
 </div>
 @if ($users->count())
-      <div class="card-body mx-auto">
-<table>
+      <div class=" container mw-100 mx-auto text-center">
+<table class=" mx-auto ">
           <thead>
                 <tr>
                     <th>ID</th>
@@ -16,16 +16,16 @@
                     <th>Perfil</th>
                 </tr>
             </thead>
-            <tbody>
+            <tbody class="text-left ">
       @foreach ($users as $user)
-      <tr  >
-        <td >{{$user->id}}</td>
-        <td>{{$user->name}}</td>
-        <td>{{$user->email}}</td>
-        <td>{{$user->phone}}</td>
-        <td>{{$user->user_type}}</td>
-        <td ><a class="btn btn-primary mx-2" href="{{route('admin.users.edit',$user)}}">Editar</a></td>
-        <td ><a class="btn btn-danger mx-2" href="{{route('admin.users.edit',$user)}}">Eliminar</a></td>
+      <tr >
+        <td class="float-left px-1 border">{{$user->id}}</td>
+        <td class="border px-1">{{$user->name}}</td>
+        <td class="border px-1">{{$user->email}}</td>
+        <td class="border px-1">{{$user->phone}}</td>
+        <td class="border px-1">{{$user->user_type}}</td>
+        <td class="px-1"><a class="btn btn-primary mx-2 w-max" href="{{route('admin.users.edit',$user)}}">Editar</a></td>
+        <td class="px-1"><a class="btn btn-danger " href="{{route('admin.users.edit',$user)}}">Eliminar</a></td>
 
         {{-- <td><a  class="btn btn-danger" href="{{route('admin.users.delete',$user)}}">Eliminar</a></td> --}}
       </tr>
